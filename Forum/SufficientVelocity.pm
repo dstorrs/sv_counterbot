a445f87f77feb9e49de19fd1c2174c0644cbeb7a
David K. Storrs <david.storrs@gmail.com>
2016-03-02 16:38:22 -0800

    Added tests and test data

---
+++ Forum/SufficientVelocity.pm
@@ -0,0 +1,15 @@
+package Forum::SufficientVelocity;
+
+use strict;
+use warnings;
+use Data::Dumper;
+use HTML::Element;
+use Log::Log4perl qw(:easy);
+
+use constant VERBOSE => 0;
+use constant BASE_URL => 'https://forums.sufficientvelocity.com/';
+
+
+###-------  Setup
+
+1;
---
+++ t/Forum/SufficientVelocity.t
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+
+use lib "../../";
+
+BEGIN { use_ok('Forum::SufficientVelocity', qw()) }
+
+
+
+done_testing();
